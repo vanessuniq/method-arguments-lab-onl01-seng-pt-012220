67f6a4b02de40a9d25c8ea3adae9d95b3549d9e9
G. Vanessa Fotso <vanessuniq@gmail.com>
2020-01-27 01:22:40 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -4,6 +4,6 @@ end
 introduction 'Josh'
 
 def introduction_with_language name, language
-  puts "Hi, my name is #{name} and I am learning to program #{language}."
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 introduction_with_language 'Vanessa', 'ruby'
